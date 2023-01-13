bb4a259a2d8de211a95940a14f6e822141f27bbf
cheol <qjel60@naver.com>
2023-01-13 13:57:47 +0900

    feat:models

--- models/index.js
+++ models/index.js
@@ -6,13 +6,27 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 
 require("./user.model")({ sequelize, DataTypes, Model });
 require("./board.model")({ sequelize, DataTypes, Model });
+require("./reply.model")({ sequelize, DataTypes, Model });
+require("./like.model")({ sequelize, DataTypes, Model });
+require("./hashTag.model")({ sequelize, DataTypes, Model });
+require("./hashTable.model")({ sequelize, DataTypes, Model });
 
-const { User, Board } = sequelize.models;
+const { User, Board, Reply, Like, HashTag, HashTable } = sequelize.models;
 User.associate(Board);
 Board.associate(User);
+Board.associatehasmany(Reply);
+Reply.associate(User);
+Reply.associateboard(Board);
+Like.associate(User);
+Like.associateboard(Board);
+HashTag.associate(HashTable);
+HashTable.associateHash(HashTag);
+HashTable.associateboard(Board);
 
 (async () => {
     await sequelize.sync({ force: true });
+    await User.create({ userid: "web7722", userpw: "1234", username: "test1" });
+    await User.create({ userid: "admin", userpw: "1234", username: "admin" });
 })();
 
 module.exports = {
